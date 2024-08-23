5c8969617c76cc73c33e4d84f6f3ba9266bfb6a5
jacobadev <junior1232131@gmail.com>
2024-08-23 03:14:58 -0300

    chore: configure deployment on railway with nixpacks and Caddyfile

--- src/domain/usecases/SummonerRepository.test.ts
+++ src/domain/usecases/SummonerRepository.test.ts
@@ -4,16 +4,16 @@ import { BlitzRepository } from './SummonerRepository'
 let repository: BlitzRepository
 
 beforeEach(() => {
-    repository = new BlitzRepository()
+  repository = new BlitzRepository()
 })
 
 test('Should return LeagueProfile', async () => {
-    const riotName = 'A Yordle#BR2'
-    const result = await repository.getLeagueProfile(riotName)
-    expect(result.status).toBe('success')
+  const riotName = 'A Yordle#BR2'
+  const result = await repository.getLeagueProfile(riotName, 'BR')
+  expect(result.status).toBe('success')
 })
 
 test('Should return MatchList', async () => {
-    const result = await repository.getMatchList()
-    expect(result.status).toBe('success')
+  const result = await repository.getMatchList()
+  expect(result.status).toBe('success')
 })
