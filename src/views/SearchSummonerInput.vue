bf22f9e4a4e873c82034138d2b317d631ef5f793
jacobadev <junior1232131@gmail.com>
2024-08-23 18:53:24 -0300

    feat: add github actions

--- src/views/SearchSummonerInput.vue
+++ src/views/SearchSummonerInput.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
-import { CaretSortIcon, CheckIcon } from '@radix-icons/vue'
+import { computed, ref } from 'vue'
 
 const summoners = [
   { value: 'next.js', label: 'Next.js' },
@@ -9,34 +8,32 @@ const summoners = [
   { value: 'remix', label: 'Remix' },
   { value: 'astro', label: 'Astro' },
 ]
+const selectedValue = defineModel<string>({ required: true })
 
 const open = ref(false)
-const value = ref('')
 const query = ref('')
 
-// Filtered summoners based on the user's query
 const filteredSummoners = computed(() =>
   summoners.filter(summoner =>
     summoner.label.toLowerCase().includes(query.value.toLowerCase()),
   ),
 )
 const showDropdown = computed(() => query.value.length > 0 && filteredSummoners.value.length > 0)
-watch(query, () => { query.value })
 function onInput(event: Event) {
   const target = event.target as HTMLInputElement
   query.value = target.value
 }
-const model = defineModel({ required: true })
 function setModel(value: string) {
-  model.value = value
+  selectedValue.value = value
   open.value = false
   query.value = ''
 }
 </script>
 
 <template>
+  {{ query }}
   <Command>
-    <CommandInput class="h-9" placeholder="Search summoner..." :value="query" @input="onInput" />
+    <Input v-model="query" placeholder="Game Name + Tag Line" class="h-9 max-w-[180px]" @input="onInput" />
 
     <CommandList v-if="showDropdown">
       <CommandGroup>
@@ -48,13 +45,3 @@ function setModel(value: string) {
     </CommandList>
   </Command>
 </template>
-
-<!-- <script setup lang="ts"> -->
-<!-- const model = defineModel<string>({ required: true }) -->
-<!---->
-<!-- </script> -->
-<!---->
-<!-- <template> -->
-<!--   <!-- <Label class="text-muted-foreground font-medium text-sm">Summoner Name</Label> --> -->
-<!--   <Input v-model="model" @update:model-value="" placeholder="Game Name + Tag Line" class="max-w-[180px]" /> -->
-<!-- </template> -->
